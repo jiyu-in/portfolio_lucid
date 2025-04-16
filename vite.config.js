901796932acada5faf4937a0a6d4f31f8ead75e6
GU <49428043+hsIN2205@users.noreply.github.com>
2025-04-17 00:58:05 +0900

    경로수정

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import svgr from 'vite-plugin-svgr';
+import { defineConfig } from 'vite';
+import htmlPlugin from 'vite-plugin-html';
 
 export default defineConfig({
-  base: '/portfolio_lucid/',
-  plugins: [react(),svgr()],
-})
+  base: '/portfolio_lucid/', // GitHub Pages 경로
+  plugins: [
+    htmlPlugin({
+      inject: {
+        // html 파일 내에서 사용할 변수를 설정할 수 있음
+        injectBase: './', // 상대 경로로 설정
+      },
+    }),
+  ],
+});
