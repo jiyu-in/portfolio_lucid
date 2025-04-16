ddea8cf5e068109972e4b60ec6188027fd2ca6a7
GU <49428043+hsIN2205@users.noreply.github.com>
2025-04-17 01:11:47 +0900

    _

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 
 export default defineConfig({
-  base: '/portfolio_lucid/', 
+  base: '/portfolio_lucid', 
   plugins: [
     react(),
     svgr(),
