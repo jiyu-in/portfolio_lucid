85608e8eb7618bfd82e81e121306200be2cce092
GU <49428043+hsIN2205@users.noreply.github.com>
2025-04-17 01:05:54 +0900

    _

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
-const htmlPlugin = require('vite-plugin-html'); // HTML 플러그인 추가
 
 export default defineConfig({
-  base: '/portfolio_lucid/', // GitHub Pages 경로 설정
+  base: '/portfolio_lucid/', 
   plugins: [
     react(),
     svgr(),
-    htmlPlugin({
-      inject: {
-        injectBase: './', // 빌드 후 리소스 경로를 상대 경로로 설정
-      },
-    }),
   ],
 })
